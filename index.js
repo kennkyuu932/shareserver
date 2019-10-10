c578719fdfa1d4d011b039c33475a529685d9132
Glitch (hello-express) <none>
2019-10-10 17:41:25 +0000

    🛩🍡 Checkpoint ./index.js:284076/643 ./message.js:284076/633

--- index.js
+++ index.js
@@ -6,9 +6,10 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const signature = require('./verifySignature');
 const axios = require('axios'); 
 const qs = require('qs');
+const signature = require('./verifySignature');
+const message = require('./message');
 
 const apiUrl = 'https://dev.slack.com/api';
 
@@ -248,25 +249,6 @@ app.post('/slack/actions', async(req, res) => {
 
 
 
-/* Calling the chat.postMessage method to send a message */
-
-const send = async(blocks, channel) => { 
-
-  const args = {
-    token: process.env.SLACK_BOT_TOKEN,
-    channel: channel,
-    blocks: blocks
-  };
-  
-  const result = await axios.post('https://slack.com/api/chat.postMessage', qs.stringify(args));
-  
-  try {
-    console.log(result.data);
-    const ts = result.data.ts;
-  } catch(e) {
-    console.log(e);
-  }
-};
 
 
 
---
+++ message.js
@@ -0,0 +1,29 @@
+const axios = require('axios'); 
+const qs = require('qs');
+
+const apiUrl = 'https://dev.slack.com/api';
+
+/*
+ *  Handling DM messages
+ */
+
+
+/* Calling the chat.postMessage method to send a message */
+
+const send = async(blocks, channel) => { 
+
+  const args = {
+    token: process.env.SLACK_BOT_TOKEN,
+    channel: channel,
+    blocks: blocks
+  };
+  
+  const result = await axios.post('https://slack.com/api/chat.postMessage', qs.stringify(args));
+  
+  try {
+    console.log(result.data);
+    const ts = result.data.ts;
+  } catch(e) {
+    console.log(e);
+  }
+};
