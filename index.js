22f3d21b10ad7c44a150c42048a42257ee903bc5
Glitch (hello-express) <none>
2019-10-10 00:13:46 +0000

    🌶👆 Checkpoint ./index.js:284076/2

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ const updateHomeView = (data) => {
       type: "section",
       text: {
         type: "mrkdwn",
-        text: "Welcome! This is a home for Stickers app. You can add small notes here by clicking the button, or DM-ing me."
+        text: "Welcome! \nThis is a home for Stickers app. You can add small notes here by clicking the button, or DM-ing me."
       },
       accessory: {
         type: "button",
