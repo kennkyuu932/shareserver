dd2294074861fbedc55c4471c3442385303efd8d
Glitch (slack-dtn-test) <none>
2021-05-14 01:43:58 +0000

    🏏👁 Checkpoint ./index.js:31276187/4

--- index.js
+++ index.js
@@ -114,8 +114,8 @@ app.post('/android', async(req, res) => {
     }
   }).then(res =>{
     //console.log(id);
-    real_name = id;
-    //real_name = res.data.user.real_name;
+    //real_name = id;
+    real_name = res.data.user.real_name;
   });
   
   const data = {
