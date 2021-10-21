d1cddba04ad750beff281fc60e6e4d5aa1a53619
Glitch (slack-dtn-test) <none>
2021-10-21 05:05:36 +0000

    🌲🏝 Checkpoint ./index.js:31276187/275

--- index.js
+++ index.js
@@ -141,7 +141,16 @@ app.post('/notice', async(req, res) => {
   const send = req.body.send;
   const receive = req.body.receive;
   
-  const time = ts.getFullYear() + `/` + (ts.getMonth() +1) + `/` + ts.getDate() + `/` + ts.getHours() + `/` + ts.getMinutes() + `/`
+  /*
+  const time = (ts.getFullYear() + `/` 
+                + (ts.getMonth() +1) + `/` 
+                + ts.getDate() + `/` 
+                + ts.getHours() + `/` 
+                + ts.getMinutes() + `/` 
+                + ts.getSeconds() + `/` 
+                + ts.getMilliseconds());
+  console.log(time);
+  */
   
   //送信通知
   if(noticeflag==3){
