35ca7f5916d292b2f3d4f31e59da6befa9a398b1
Glitch (slack-dtn-test) <none>
2021-10-22 04:32:29 +0000

    🐞🍰 Checkpoint ./notification.json:31276187/357 ./index.js:31276187/11478

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-/* 
- * Slack API Demo 
+/*
+ * Slack API Demo
  * This example shows how to ustilize the App Home feature
  * October 11, 2019
  *
@@ -7,26 +7,25 @@
  * To see how this can be written in Bolt, https://glitch.com/edit/#!/apphome-bolt-demo-note
  */
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const axios = require('axios'); 
-const qs = require('qs');
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+const qs = require("qs");
 
-const signature = require('./verifySignature');
-const appHome = require('./appHome');
-const message = require('./message');
+const signature = require("./verifySignature");
+const appHome = require("./appHome");
+const message = require("./message");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 
-const apiUrl = 'https://slack.com/api';
+const apiUrl = "https://slack.com/api";
 
 //notification.jsonに書き込むための準備(2021/10/21追加)
-const JsonDB = require('node-json-db');
-const notice = new JsonDB('notification', true, false);
+const JsonDB = require("node-json-db");
+const notice = new JsonDB("notification", true, false);
 //
-
 
 /*
  * Parse application/x-www-form-urlencoded && application/json
@@ -38,68 +37,64 @@ const notice = new JsonDB('notification', true, false);
 
 const rawBodyBuffer = (req, res, buf, encoding) => {
   if (buf && buf.length) {
-    req.rawBody = buf.toString(encoding || 'utf8');
+    req.rawBody = buf.toString(encoding || "utf8");
   }
 };
 
-app.use(bodyParser.urlencoded({verify: rawBodyBuffer, extended: true }));
+app.use(bodyParser.urlencoded({ verify: rawBodyBuffer, extended: true }));
 app.use(bodyParser.json({ verify: rawBodyBuffer }));
-
 
 /*
  * Endpoint to receive events from Events API.
  */
 
-app.post('/slack/events', async(req, res) => {
+app.post("/slack/events", async (req, res) => {
   switch (req.body.type) {
-      
-    case 'url_verification': {
+    case "url_verification": {
       console.log("VERIFICATION");
       // verify Events API endpoint by returning challenge if present
       res.send({ challenge: req.body.challenge });
       break;
     }
-      
-    case 'event_callback': {
+
+    case "event_callback": {
       console.log("CALLBACK");
       // Verify the signing secret
       if (!signature.isVerified(req)) {
         res.sendStatus(404);
         return;
-      } 
-      
+      }
+
       // Request is verified --
       else {
-        
-        const {type, user, channel, tab, text, subtype} = req.body.event;
+        const { type, user, channel, tab, text, subtype } = req.body.event;
         const team_id = req.body.team_id;
 
         // Triggered when the App Home is opened by a user
-        if(type === 'app_home_opened') {
+        if (type === "app_home_opened") {
           // Display App Home
           appHome.displayHome(user, team_id);
         }
       }
       break;
     }
-    default: { res.sendStatus(404); }
+    default: {
+      res.sendStatus(404);
+    }
   }
 });
-
-
 
 /*
  * Endpoint to receive an button action from App Home UI
  */
 
-app.post('/slack/actions', async(req, res) => {});
-
+app.post("/slack/actions", async (req, res) => {});
 
 /*
  * Endpoint to receive an action from Android app "DTNFileShare"
  */
 
-app.post('/android', async(req, res) => {
+app.post("/android", async (req, res) => {
   console.log("form android");
   res.send("android");
 
@@ -107,60 +102,68 @@ app.post('/android', async(req, res) => {
   const id = req.body.id;
   const eid = req.body.eid;
   const team_id = req.body.team_id;
-  
+
   var real_name;
-  await axios.get(`${apiUrl}/users.info`, {
-     params: {
-	 // token: process.env.SLACK_BOT_TOKEN,
-       user: id
-     },
-     headers: {
-       Authorization: `Bearer ${process.env.SLACK_BOT_TOKEN}`
-     }
-  }).then(res =>{
-    real_name = res.data.user.real_name;
-  });
-  
+  await axios
+    .get(`${apiUrl}/users.info`, {
+      params: {
+        // token: process.env.SLACK_BOT_TOKEN,
+        user: id
+      },
+      headers: {
+        Authorization: `Bearer ${process.env.SLACK_BOT_TOKEN}`
+      }
+    })
+    .then(res => {
+      real_name = res.data.user.real_name;
+    });
+
   const data = {
     id: id,
     eid: eid,
     real_name: toCodepoint(real_name)
-  }
-  
-  appHome.displayHome(id, team_id, data);
-})
+  };
 
+  appHome.displayHome(id, team_id, data);
+});
 
 //Slack通知の外部出力(2021/10/21追加)
-app.post('/notice', async(req, res) => {
+app.post("/notice", async (req, res) => {
   console.log("web api");
   res.send("android");
 
-  const tim = new Date(Date.now() + ((new Date().getTimezoneOffset() + (9 * 60)) * 60 * 1000));
+  const tim = new Date(
+    Date.now() + (new Date().getTimezoneOffset() + 9 * 60) * 60 * 1000
+  );
   //console.log(tim);
-  const ts = (tim.getFullYear() + `/` 
-          + (tim.getMonth() +1) + `/` 
-          + tim.getDate() + ` ` 
-          + tim.getHours() + `:` 
-          + tim.getMinutes() + `:` 
-          + tim.getSeconds() + `.` 
-          + tim.getMilliseconds());
+  const ts =
+    tim.getFullYear() +
+    `/` +
+    (tim.getMonth() + 1) +
+    `/` +
+    tim.getDate() +
+    ` ` +
+    tim.getHours() +
+    `:` +
+    tim.getMinutes() +
+    `:` +
+    tim.getSeconds() +
+    `.` +
+    tim.getMilliseconds();
   const noticeflag = req.body.noticeflag;
   const send = req.body.send;
   const receive = req.body.receive;
-  
-  
+
   try {
     if (notice.getData(`/`).dtn === undefined) {
-      notice.push(`/`, {notice: []});
+      notice.push(`/`, { notice: [] });
       notice.save();
       notice.reload();
     }
-  } catch(error) {
+  } catch (error) {
     console.error(error);
-  };
+  }
 
-  
   /*
   const time = (ts.getFullYear() + `/` 
                 + (ts.getMonth() +1) + `/` 
@@ -171,25 +174,24 @@ app.post('/notice', async(req, res) => {
                 + ts.getMilliseconds());
   console.log(time);
   */
-  
+
   //送信通知
-  if(noticeflag==3){
+  if (noticeflag == 3) {
     const message = req.body.message;
     console.log("送信通知");
-  
+
     const data = {
       time: ts,
       send: send,
       receive: receive,
       message: message
-    }
-    
-    
-    notice.push(`/notice[]`,data,true);
+    };
+
+    notice.push(`/notice[]`, data, true);
     notice.save();
     notice.reload();
     //console.log(notice.getData());
-    
+
     /*
     console.log(ts);
     console.log(send);
@@ -198,21 +200,20 @@ app.post('/notice', async(req, res) => {
     */
   }
   //受信通知
-  if(noticeflag==4){
-    const bundleid=req.body.bunid;
+  if (noticeflag == 4) {
+    const bundleid = req.body.bunid;
     console.log("受信通知");
     const data = {
       time: ts,
       send: send,
       receive: receive,
       bundleid: bundleid
-    }
-    
-    
-    notice.push(`/notice[]`,data,true);
+    };
+
+    notice.push(`/notice[]`, data, true);
     notice.save();
     notice.reload();
-    
+
     //console.log(notice.getData());
 
     /*
@@ -222,30 +223,29 @@ app.post('/notice', async(req, res) => {
     console.log(bundleid);
     */
   }
-
-  
-
-  
-})
+});
 //
-
 
 /* Running Express server */
 const server = app.listen(5000, () => {
-  console.log('Express web server is running on port %d in %s mode', server.address().port, app.settings.env);
+  console.log(
+    "Express web server is running on port %d in %s mode",
+    server.address().port,
+    app.settings.env
+  );
 });
 
-
-app.get('/', async(req, res) => {
-  res.send('Hello, There is no web UI for this code sample. To view the source code, click "View Source"');
+app.get("/", async (req, res) => {
+  res.send(
+    'Hello, There is no web UI for this code sample. To view the source code, click "View Source"'
+  );
 });
-
 
 /* unidode encoder */
-const toCodepoint = (str) => {
+const toCodepoint = str => {
   var result = "";
   for (var i = 0; i < str.length; i++) {
-    result = result + "\\u" + str.codePointAt(i)
+    result = result + "\\u" + str.codePointAt(i);
   }
   return result;
-}
+};
